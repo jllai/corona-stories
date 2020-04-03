fa5447f5f08be703da57ab7fc06db101c6deb248
ackerm <ackerm@gmail.com>
2020-04-02 20:04:02 -0400

    app.js index.html
    Fixed bug on story number and timestamp, added spans (with class ids) for the footer, removed buttons in footer

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 Vue.component('story', {
-    props: ['headline', 'subtitle', 'postTime', 'index'],
+    props: ['headline', 'subtitle', 'postTime', 'index','number'],
     template: `<div>
         <v-card class="vcard-inner">
-            <v-card-title class="headline">{{headline }} {{ index }}</v-card-title>
+            <v-card-title class="headline">{{headline }} {{ number }}</v-card-title>
 
-            <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
-
+        <v-card-subtitle class="vcard-subtitle">{{ subtitle }}</v-card-subtitle>
+	<v-card-footer class="footer">
+	<span class="footer-left">{{ postTime }}</span>
+	<span class="footer-right">story {{ number }}</span>
+	</v-card-footer>
             <v-card-actions>
-                <div class="left-button"><v-btn text>{{ postTime }}</v-btn></div> <div class="right-button"><v-btn class="story" text>{{ postTime }} {{ index }}</v-btn></div>
             </v-card-actions>
         </v-card>
     </div>`
@@ -29,7 +31,8 @@ new Vue({
                 headline: 'My story',
                 subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 postTime: 'March 20, 2020  18:59',
-                index: index
+                index: index,
+		number:number
             }
         })
     }
